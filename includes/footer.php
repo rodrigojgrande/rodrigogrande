3cd78e3f9969cb187d986f1f0c43acdcd1dd1eb0
Rodrigo Grande <rodrigojgrande@gmail.com>
2021-08-28 10:15:46 -0300

    style: organizing css

--- includes/footer.php
+++ includes/footer.php
@@ -1,7 +1,7 @@
 	<!-- JavaScript Bundle with Popper -->
 	<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js"></script>
 
-    <!-- CKeditor -->
+  <!-- CKeditor -->
 	<!-- <script src="https://cdn.ckeditor.com/ckeditor5/29.0.0/classic/ckeditor.js"></script> -->
 
   <!-- Script -->
--- index.php
+++ index.php
@@ -7,20 +7,12 @@ $description = '<meta charset="utf-8" name="description" content="Site em Constr
 <?php include_once "includes/header.php" ?>
 
 <div id="particles-js">
-    <h1 class="position-absolute text-center display-1 text-white" style="position: absolute;
-        top: 50%;
-        left: 50%;
-        margin-right: -50%;
-        transform: translate(-50%, -50%)">Em Construção</h1>
-    <p class="position-absolute btn bg-dark text-center text-white" style="position: absolute;
-        top: 70%;
-        left: 50%;
-        margin-right: -50%;
-        transform: translate(-50%, -50%)">por <a href="https://www.instagram.com/rodrigojgrande/" rel="noreferrer noopener" class="text-decoration-none text-white" title="Instagram do Rodrigo Grande">Rodrigo Grande</a></p>
+    <h1 class="position-absolute text-center display-1 text-white center-main-text">Em Construção</h1>
+    <p class="position-absolute btn bg-dark text-center text-white center-sub-text">por <a href="https://www.instagram.com/rodrigojgrande/" rel="noreferrer noopener" class="text-decoration-none text-white" title="Instagram do Rodrigo Grande">Rodrigo Grande</a></p>
 </div>
 
 <!-- scripts -->
-<script src="js/particles.js"></script>
+<script src="js/particles.min.js"></script>
 <script src="js/app.js"></script>
 
 
