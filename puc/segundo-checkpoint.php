11ba85d6200d0b2188a30143fc07a7fd77cc4484
Rodrigo Grande <rodrigojgrande@gmail.com>
2023-12-15 18:47:50 -0300

    Mentoring - Third Checkpoint

---
+++ puc/segundo-checkpoint.php
@@ -0,0 +1,173 @@
+<?php
+$pathFiles = "../";
+$titlePage = "PUC | Mentoring I";
+$description = '<meta charset="utf-8" name="description" content="Página dedicada para trabalho de Mentoring I - Ciência da Computação - PUC Poços de Caldas">';
+?>
+
+<?php include_once($pathFiles . "includes/header.php") ?>
+
+<main class="container-fluid bg-white w-100 h-100 text-black">
+    <div class="row bg-white p-5">
+        <h1 class="text-center display-1">MENTORING I</h1>
+        <figure class="text-center">
+            <h2>Página dedicada ao Professor <br>Joao Carlos de Moraes Morselli Junior</h2>
+            <figcaption class="blockquote-footer pt-2">AKA Boca</figcaption>
+        </figure>
+
+        <h3 class="pt-3 display-2">Checkpoint II</h3>
+        <figure>
+            <figcaption class="blockquote-footer pt-2">Vou ser um pouco mais transparente aqui</figcaption>
+        </figure>
+
+            <h4 class="pt-3">História de Vida</h4>
+            <h5 class="pt-3">Como você descreveria sua infância e adolescência?</h5>
+            <p>
+                Na infância, me lembro de ficar muito sozinho. Gosto de dizer que muita da minha criação foi dada por um vídeo game.<br>
+                Minha mãe não deixava eu sair pra brincar, a diferença entre a idade de mim e meus irmãos é considerável, então ficava sozinho em casa dado os horários e tudo.<br>
+                Muito do meu inglês e meu raciocínio lógico veio por conta desse período.<br>
+                Ainda na juventude, teve a questão de me tornar obeso, o que para eu com uma estatura baixa e gordo, era sempre alvo de chacota por todos.<br>
+            </p>
+
+            <h5 class="pt-3">Quais foram os momentos mais marcantes em sua vida até agora?</h5>
+            <p>
+                Acho que o mais marcante foi a morte do meu melhor amigo. Nunca me esqueço do abraço que eu dei na mãe dele e no irmão mais velho dele.<br>
+                Teve a vez que meu pai tentou agredir a minha mãe e eu e meu irmão (do meio) tivemos que partir para violência para apartar a situação.<br>
+                Teve também a situação de uma pessoa que eu chamava de melhor amigo, mentiu na minha cara sobre uma questão envolvendo a minha ex na época. Isso abalou bastante minha confiança.<br>
+            </p>
+
+            <h5 class="pt-3">Há eventos significativos que acredita terem influenciado sua personalidade?</h5>
+            <p>
+                Creio que na juventude, muita da minha personalidade foi dada pelos meus irmãos e depois na adolescência até a vida adulta, namorei uma moça que através do convívio com ela, minha personalidade se alterou bastante.<br>
+            </p>
+
+            <h4 class="pt-5">Relacionamentos</h4>
+            <h5 class="pt-3">Como é sua relação com a família?</h5>
+            <p>
+                Por conta das brigas do meu pai e da minha mãe, eu e meus irmãos ficamos muito próximos. Creio que a relação entre nós é uma coisa muito especial, difícil encontrar irmãos com um vínculo tão forte assim.<br>
+                Tenho uma boa relação com a minha mãe, ela é meio "doidinha", mas é um amor de pessoa. Têm a questão dela ter uma mentalidade de uma jovem de 13 anos de idade então, bom, é complicado.<br>
+                Sobre o meu pai, ainda tenho contato com ele. Não tenho nenhum vínculo forte, uma relação sociável, ele me ajuda com caronas para Pouso Alegre e supermercado algumas vezes.<br>
+            </p>
+
+            <h5 class="pt-3">Como você se sente em relação a amizades e relacionamentos românticos?</h5>
+            <p>
+                Sempre fui uma pessoa sociável, quando estudava eu conversava com todos na sala mas me lembro de ter dificuldade em criar laços fortes.<br>
+                Meu núcleo de amigos na vida adulta meio que se rompeu por conta de várias intrigas.<br>
+                Quanto a relacionamentos, namorei a mesma moça por 10 anos desde dos meus 15 anos de idade.<br>
+                Depois, namorei uma outra por quase 1 ano mas por conta de eu passar na faculdade, acabamos rompendo (história longa).<br>
+                Minha vinda aqui pra Poços foi uma oportunidade pra eu estar recomeçando minha vida aqui.<br>
+            </p>
+
+            <h4 class="pt-5">Educação e Carreira</h4>
+            <h5 class="pt-3">Como foi sua experiência na escola?</h5>
+            <p>
+                Quando criança eu era bem "nerdinho", tirava nota máxima em tudo.<br>
+                Na adolescência eu fiquei um pouco mais desleixado, morava em um prédio cheio de moleque então eu ficava mais brincando do que estudando.<br>
+                Não me lembro de estudar, fazer exercícios mas sempre tive sagacidade pra entender o conteúdo rapidamente.<br> 
+                Me lembro que em sala de aula eu era muito distraído e têm a questão dos professores também não terem uma didática que colaborasse.<br>
+                Me lembro que quando eu estava gordinho, mesmo fazendo muito calor eu sempre estava de blusa por vergonha do meu corpo.<br>
+            </p>
+
+            <h5 class="pt-3">Como se sente em relação à sua carreira atual ou aspirações profissionais?</h5>
+            <p>
+                Até o momento, creio que venho construindo um currículo interessante.<br>
+                Fiz dois cursos técnicos na área de TI, dois estágios, trabalhei por 5 anos na área e agora estou cursando uma faculdade.<br>
+                Já fiz vários cursos, tanto em TI quanto em outras áreas (entra no conceito de Dev em T).<br>
+                Espero achar agora qual área eu vou me identificar e especializar cursando a faculdade.<br>
+            </p>
+
+            <h4 class="pt-5">Desafios e Conquistas</h4>
+            <h5 class="pt-3">Quais foram os maiores desafios que enfrentou?</h5>
+            <p>
+                Penso que fazer a virada pro EAD durante na Faculdade que eu trabalhava foi um baita desafio, todo esse período de pandemia foi bastante desafiador.<br>
+            </p>
+            <h5 class="pt-3">Quais são suas maiores conquistas até agora?</h5>
+            <p>
+                Não consigo pensar direito em "conquistas", eu desenvolvi muito meu intelecto, comprei algumas coisas que eu sempre sonhei como meu instrumento, minha câmera fotográfica, montei meu PC por completo.<br>
+                Fora isso, acho que algo que eu fico muito contente foi ter pago a cirurgia da minha mãe e ter ajudado a minha família durante a pandemia.<br>
+            </p>
+
+            <h4 class="pt-5">Valores e Crenças</h4>
+            <h5 class="pt-3">Quais são seus valores fundamentais na vida?</h5>
+            <p>Respeito e Sinceridade, são dois valores que prezo muito.</p>
+            <h5 class="pt-3">Existem crenças ou filosofias que você considera importantes?</h5>
+            <p>
+                Sigo a ideia de Absurdismo do Camus;<br>
+                Gosto bastante do Zen Budismo, Estoicismo, Bushido, tenho um forte apreço por filosofia e gosto de conhecer, estudar.
+            </p>
+
+            <h4 class="pt-5">Saúde Mental e Emocional</h4>
+            <h5 class="pt-3">Como você avalia sua saúde mental e emocional atualmente?</h5>
+            <p>
+                Ansioso, mas melhor. Por vezes, as coisas da faculdade e vida pessoal acabam acumulando e me deixando bem ansioso mas estou melhor do que antes de entrar na faculdade.<br>
+                Começo do ano passado eu passei por uma crise depressiva, estava tomando remédio e tudo. Depois que entrei na faculdade, melhorei consideravelmente ao ponto de largar a medicação.<br>
+            </p>
+            <h5 class="pt-3">Já buscou apoio psicológico antes? Como foi a experiência?</h5>
+            <p>
+                Sim, comecei buscando por apoio psicológico para um melhor entendimento sobre mim mesmo. Depois fiz um bom tempo de terapia, ainda mais quando entrei em crise depressiva.
+                Teve experiências ruins e outras muito boas. Passei por psiquiatra e por terapias não tão boas mas também passei por pessoas muito boas também. Terapia é algo que recomendo a todos.
+            </p>
+
+            <h4 class="pt-5">Interesses e Atividades</h4>
+            <h5 class="pt-3">Quais são seus hobbies e interesses?</h5>
+            <p>
+                Gosto bastante de jogar games, xadrez; <br>
+                Assistir filmes, animações; <br>
+                Fazer atividade física como jogar futsal, academia;<br>
+                Gostava bastante de andar de bicicleta e treinar Muay Thai; <br>
+                Gosto de ficar tocando baixo;<br>
+                Sair, viajar é algo que me agrada muito, experiências novas;<br>
+                Também gosto de fotografia, audiovisual, brincar um pouco com motion<br>
+            </p>
+
+            <h4 class="pt-5">Metas e Planos Futuros</h4>
+            <h5 class="pt-3">Quais são seus objetivos a curto e longo prazo?</h5>
+            <p>
+                Seria terminar a faculdade, arrumar um trabalho, investir em alguns equipamentos, ferramentas para melhorar o meu workflow e tirar carteira.
+                A longo prazo seria arrumar uma casa pra mim e tentar viajar bastante.<br>
+            </p>
+
+            <h4 class="pt-5">Autoimagem e Autoestima</h4>
+            <h5 class="pt-3">Como você se vê? Existe uma diferença entre a sua autoimagem e a forma como os outros o percebem?</h5>
+            <p>
+                Na terapia eu consegui melhorar a minha questão da autoestima, sempre tive a autoestima baixa e aprendi a reconhecer o meu valor.<br>
+                Uma questão ainda que persiste é quanto ao meu corpo, por vezes ainda me sinto meio "gordinho" enquanto todo mundo fala que estou bem magro.<br>    
+            </p>
+
+            <h4 class="pt-5">Mudanças Desejadas</h4>
+            <h5 class="pt-3">Existem áreas específicas da sua vida que gostaria de melhorar?</h5>
+            <p>Gostaria de melhor a questão do meu foco, por vezes eu procrastino bastante.<br></p>
+            <h5 class="pt-3">Como enxerga o papel do acompanhamento psicológico nesse processo?</h5>
+            <p>Importante, penso em usar os serviços pra PUC pra auxiliar nesse processo.<br></p>
+
+            <h4 class="pt-5">Sobre a PUC</h4>
+            <p>
+                Quanto a faculdade têm sido uma experiência bem legal no geral.<br>
+
+                Único ponto que comecei a ficar um pouco preocupado seria se teríamos disciplinas voltadas para a cientificidade em si, como é o pensar cientifico e como se faz ciência. A forma correta de se pesquisar e tudo.<br>
+
+                Sobre a sala em si, o pessoal é bem tranquilo. O pessoal com quem ando é bem gente boa, creio que fechei um grupo com a galera certa. Única coisa que eu sinto é o deslocamento por conta da idade, eu e mais dois na sala somos os mais velhos enquanto a grande maioria acabou de sair do Ensino Médio.<br>
+
+                Curioso que esse sentimento se repete em vários momentos na faculdade, até em eventos e festas. Há uma diferença considerável na idade e com isso, a minha mentalidade difere bastante do pessoal.<br>
+
+                Sobre as disciplinas, cálculo eu acabei indo mal na primeira prova, por mais que eu estudei, chegou na hora da prova eu acabei me perdendo, eu acabo complicando as coisas mais do que deveria e tomando um rumo nas questões que não deveria. <br>
+
+                Estou com um certo receio de não conseguir passar em cálculo apesar dos meus esforços, vou tentar ao máximo.<br>
+
+                No restante das disciplinas está bem tranquilo ainda, como eu já tinha um conhecimento prévio está sendo bem tranquilo, agora em AED que comecei a ver conteúdos que eu nunca havia visto antes.<br>
+
+                Final de semestre acumulou bastante trabalho de várias disciplinas, tentando equilibrar tudo isso e priorizando os trabalhos de cálculo.<br>
+
+                LPHS eu fico meio boiando na questão da montagem dos circuitos, acabo ficando mais no código.<br>
+
+                Ainda fico meio perdido no portal em como se inscrever nos eventos que têm na faculdade, oficinas, etc.<br>
+
+                Participando dos eventos que teve na faculdade, Olimpuc, campeonato de X1, percebo que o pessoal da Ciência da Computação têm uma presença bem a desejar, espero que isso melhore.<br>
+
+                O Seminário que teve de HTML, CSS e JavaScript ficou bem a desejar. Em uma semana de aula daria pra elaborar algum projeto bem interessante e mais interativo com o pessoal.<br>
+
+            </p>
+
+    </div><!-- /row-->
+</main>
+
+<?php include_once($pathFiles . "includes/footer.php") ?>
---
+++ puc/terceiro-checkpoint.php
@@ -0,0 +1,128 @@
+<?php
+$pathFiles = "../";
+$titlePage = "PUC | Mentoring I";
+$description = '<meta charset="utf-8" name="description" content="Página dedicada para trabalho de Mentoring I - Ciência da Computação - PUC Poços de Caldas">';
+?>
+
+<?php include_once($pathFiles . "includes/header.php") ?>
+
+<main class="container-fluid bg-white w-100 h-100 text-black">
+    <div class="row bg-white p-5">
+        <h1 class="text-center display-1">MENTORING I</h1>
+        <figure class="text-center">
+            <h2>Página dedicada ao Professor <br>Joao Carlos de Moraes Morselli Junior</h2>
+            <figcaption class="blockquote-footer pt-2">AKA Boca</figcaption>
+        </figure>
+
+        <h3 class="pt-3 display-2">Checkpoint III</h3>
+        <figure>
+            <figcaption class="blockquote-footer pt-2">Falar agora só sobre a minha experiência na PUC</figcaption>
+        </figure>
+
+            <h4 class="pt-3">PUC</h4>
+            <h5 class="pt-3">Experiência no primeiro período</h5>
+            <p>
+                Primeiro período chegou ao fim 😱<br>
+                Parece que foi ontem que eu estava fazendo minha matrícula na secretaria na PUC, pisquei o tempo passou voando.<br>
+                Como eu havia dito na primeira etapa desse projeto, meu plano era que meu site estivesse funcional nessa etapa (a ideia era que na etapa anterior fosse feito o projeto dele no Figma e na próxima à implementação).<br>
+                Bora fazer uma recapitulação desse Semestre:<br>
+                Bom, desde do início eu sabia que a disciplina que mais ia me pegar nesse primeiro momento seria cálculo, e de fato, eu estava certo.<br>
+                Por conta de eu já ter experiência e formação na área, acabou que não vi nada de "muito novo" nesse primeiro contato, mas ainda, sim, acabei por ver certos conceitos de uma forma mais profunda.<br>
+                Outros como Arduino, portas lógicas, ponteiros, structure, a própria linguagem C, era algo que eu não havia tido contato.<br>
+                Vou separar a conversa por tópicos aqui:<br>
+            </p>
+
+            <h5 class="pt-5">DA</h5>
+            <p>
+                <b>Observação:</b> Tenho ciência também que estava acontecendo a troca de chapa enquanto estavámos começando a faculdade e isso pode ter afetado a nossa experiência.<br>
+                Por conta de que eu sou mais velho com o pessoal, e têm o fator que eu sou extremamente organizado (beirando ao TOC), achei que várias coisas ficaram deixando a desejar:<br>
+                A recepção foi bem fraca para os calouros, demorou muito pra um representante do curso estar vindo falar conosco.<br>
+                Era comum ver a maioria dos DAs em funcionamento enquanto o do nosso curso estava boa parte do tempo fechado.<br>
+                Achei que a nossa articulação do curso foi bem fraca, talvez por um descaso ou por uma má organização, nos eventos que existiam nós não marcávamos muita presença.<br>
+            </p>
+
+            <h5 class="pt-5">LPHS - Celso</h5>
+            <p>
+                É uma das disciplinas mais interessantes, pois é muito palpável, sai um pouco da abstração e vêm para o mundo físico.<br>
+                No ensino médio eu não tive as aulas física que falam sobre energia, etc. Tenho uma grande lacuna nessa questão e imaginei que talvez nessa disciplina eu teria algo sobre isso.<br>
+                O professor é ótimo, qualquer dúvida que tínhamos ele sempre foi muito solista em ajudar. O estilo da aula é meio que "solto", sem muito material de apoio, mas creio eu que seja essa a ideia.<br>
+            </p>
+
+            <h5 class="pt-5">LAED - Claudio</h5>
+            <p>
+                Para mim foi tranquilo por que eu já programava, então a questão prática da programação foi bem tranquilo.<br>
+                Sempre que eu trazia alguma pergunta ao professor ele sempre respondeu, respondeu rápido no portal e tal.<br>
+                Tinha a expectativa que programaríamos mais nessa disciplina, talvez uma questão de outra abordagem quanto ao conteúdo dado seria mais interessante.<br>
+            </p>
+
+            <h5 class="pt-5">AED - João Benedito</h5>
+            <p>
+                Fiquei bem surpreso aqui, de uma maneira positiva. Questão de ver conteúdos já conhecidos, mas agora de uma maneira mais aprofundada.<br>
+                O professor é espetacular, têm uma didática muito boa, transmite bem o conteúdo e sempre muito atencioso em todos os pontos.<br>
+            </p>
+
+            <h5 class="pt-5">ICC - Fabiano</h5>
+            <p>
+                Tive a sensação de deslumbre aqui porque sabia que nessa disciplina era um primeiro contato com os tópicos que veríamos de maneira aprofundada no curso.<br>
+                O professor também têm uma didática fantástica, uma habilidade de oratória sensacional.<br>
+            </p>
+
+            <h5 class="pt-5">CALC - Marcio</h5>
+            <p>
+                A disciplina que mais me gastou nesse semestre, de longe, foi cálculo. Meu respeito pelos engenheiros e por todos os que passam por essa disciplina aumentou consideravelmente.<br>
+                Fui bem mal na primeira prova tirando 7,5/25, mas recuperei agora na segunda, quase gabaritando a prova tirando 23,5/25 😤<br>
+                Meu estresse foi lá em cima por conta dessa matéria, comecei a ter espasmos na pálpebra esquerda. Me empenhei bastante para recuperar os pontos que perdi na primeira prova (movido a base de medo e ódio quanto a uma possível DP).<br>
+                O professor de longe foi o melhor que eu já tive quanto a matemática, explica muito bem e sempre foi muito atencioso quanto a dúvida que trazíamos, sempre ficava à disposição depois da aula para dúvidas,
+                canvas muito bem organizado, cheio de material auxiliar.<br>
+            </p>
+
+            <h5 class="pt-5">SEMINÁRIO</h5>
+            <p>
+                Acho que foi meio unânime que o seminário I foi bem decepcionante. Acho que talvez o professor não era nem ruim, mas toda a escolha de material e abordagem do tema, proposta, foi tudo bem fraco.<br>
+                Com o tema ainda de HTML, CSS e JavaScript poderia ter feito algo muito interessante, ter ficado apenas em formulário foi algo meio frustrante.<br>
+            </p>
+
+            <h5 class="pt-5">OLIMPUC</h5>
+            <p>
+                Foi um evento espetacular de estar participando, acho que faltou só um peso maior da galera da computação no evento, mas até entendo que o perfil também do nosso curso não coincide muito com esse tipo de evento.<br>
+                Ficaram faltando registros do pessoal da Computação, os meninos do basquete chegaram na final, foi bem massa. Faltaram umas fotos, uma organização melhor para estar marcando mais presença nesses eventos.<br>
+            </p>
+
+            <h5 class="pt-5">X1 FUTSAL</h5>
+            <p>
+                Representei na linha a Computação nesse evento, o evento foi bacana, mas me pareceu não tão bem organizado, pessoal da computação também não colaborou muito, o goleiro que estava comigo nem era goleiro realmente e quebrou o galho pra gente (era o Zappa) e o reserva que falou que vinha nem colou.<br>
+                Sem contar que só fui eu e o Zappa no evento, ninguém mais da computação apareceu. Ganhamos da Medicina de 3x0 e perdemos para a Engenharia Elétrica por 3x1 se não me engano (acabei ficando muito cansado, reserva fez falta).<br>
+            </p>
+
+            <h5 class="pt-5">ECOMP</h5>
+            <p>
+                Aconteceu que o fim de semestre foi bem puxado mesmo, a questão de cálculo deu uma acabada comigo, sem contar que teve ECOMP também onde eu estava apoiando o pessoal e agora preciso finalizar o material do evento.<br>
+                O evento foi bem massa, tive a oportunidade de conhecer uma galera que me parece ser bem gente boa e colaborar pra cena de e-ports e games na região.<br>
+                Minha utopia, sonho, seria ver um time da PUC representando em campeonatos afora. Que a PUC reconhecesse o valor desses esportes e investissem nos atletas pra estarem levando o nome da instituição pra fora.<br>
+            </p>
+
+            <h5 class="pt-5">CONSIDERAÇÕES FINAIS</h5>
+            <p>
+                Certas coisas de como funciona na faculdade ainda fiquei meio confuso, como, por exemplo, a atlética, o DA, essas oficinas.<br>
+                Uma dor que ficou muito latente é a questão da prática de esportes, depois da Olimpuc, o pessoal da computação parou de se organizar pra jogar na quadra da faculdade. Eles reúnem pra jogar <br>
+                society e tudo, o que é bem legal, mas a questão é que nessa situação, é tudo pago e a minha condição financeira não é nada favorável pra ficar pagando pra jogar, sendo que a própria faculdade oferece uma quadra pra gente.<br>
+                E nisso, me faz pensar que não devo ser o único com essa dor, de estar querendo jogar, mas não conseguir um horário com uma galera, o que me parece meio absurdo estando dentro de uma universidade.<br>
+                Tentei contato com a atlética também, mas o pessoal parece não ter uma boa organização também, então ficou elas por elas. Provavelmente vou tentar organizar esse pessoal por mim mesmo.<br>
+                Ainda imagino um evento de futsal na PUC entre professores contra alunos, como um evento mais pra descontrair no curso, seria bem massa ver isso.<br>
+                Achei sensacional a questão de vagas de trabalho/estágio no grupo do curso, isso acaba ajudando bastante.<br>
+                Na questão de acesso aos eventos, oficinas, acho ainda um tanto quanto confuso fazer as inscrições e participar dos eventos. Teve uma vez que você mandou o link da aula no Teams no grupo e achei uma mão na roda, menos uma barreira pra acessar um evento muito bom (princípio de design e marketing).<br>
+                Queria trazer a minha banda pra tocar na PUC ou em alguma festa da faculdade.<br>
+                Estou bastante curioso pelas próximas disciplinas, mergulhar mais fundo nessas águas da Ciência da Computação e no fim, emergir um cientista.<br>
+            </p>
+
+    </div><!-- /row-->
+    <nav class="navbar fixed-bottom bg-light">
+         <div class="container-fluid d-flex justify-content-around">
+            <a href="index.php">Primeiro Checkpoint</a>
+            <a href="segundo-checkpoint.php">Segundo Checkpoint</a>
+            <a href="terceiro-checkpoint.php">Terceiro Checkpoint</a>
+         </div>
+    </nav>
+</main>
+
+<?php include_once($pathFiles . "includes/footer.php") ?>
