b05306f698fad26e3ee566ba584fd32861e276f7
Rodrigo Grande <rodrigojgrande@gmail.com>
2024-06-27 22:52:55 -0300

    Mentoring II

---
+++ puc/mentoring.php
@@ -0,0 +1,33 @@
+<?php
+$pathFiles = "../";
+$titlePage = "PUC | Mentoring II";
+$description = '<meta charset="utf-8" name="description" content="Página dedicada para trabalho de Mentoring I - Ciência da Computação - PUC Poços de Caldas">';
+?>
+
+<?php include_once($pathFiles . "includes/header.php") ?>
+
+<main class="container-fluid bg-white w-100 h-100 text-black">
+    <div class="row bg-white p-5">
+        <h1 class="text-center display-1">MENTORING II</h1>
+        <figure class="text-center">
+            <p>por Rodrigo Grande</p>
+        </figure>
+
+        <h2 class="pt-3 display-2">Review da Disciplina</h2>
+
+        <p class="h3">
+            O conteúdo estava bem estruturado, o portal desde do início sempre esteve muito bem organizado, facilitando a compreensão das atividades.<br><br>
+            Os assuntos e tópicos em salas de aula eram relevantes e atuais, foi interessante poder conversar sobre certos temas abordados.<br><br>
+            Achei bem interessante o uso do Github durante a disciplina, é uma ferramenta muito completa, mas acaba que por desconhecimento, não usamos todos os recursos que a plataforma proporciona. Essa experiência de explorar mais a ferramenta poderia ser mais abordada na disciplina, capacitando os alunos para uma maior familiaridade com essa ferramenta tão popular no mercado.
+            <br><br>
+            Tenho certas críticas quanto ao método de avaliação pelo Github, se estiver sendo verificado a atividade do aluno apenas pelo número de commit talvez esteja errado esse método. A pessoa pode colocar um ponto e vírgula e salvar a edição que consta como commit mas não necessariamente ela se empenhou no trabalho, assim como apenas o fato dela ter feito um commit não significa que ela de fato fez algo no trabalho. Imagino que seja um processo complicado fazer a avaliação dessas atividades.
+            <br><br>
+            Outro ponto que me questionei seria se foi muito cedo para uma disciplina extencionista, talvez o ideal seria um estado mais avançado no curso para falar com maior propriedade para outras pessoas sobre impactos da tecnologia, talvez assim diminuindo um pouco o risco de cometer alguma gafe e sim falar com maior propriedade sobre temas relacionados a tecnologia.
+            <br><br>
+            Pelo fato da disciplina ser em grupo e proporcionar rodas de conversações, foi interessante pra conhecer e conversar mais com alunos da sala.<br><br>
+            Outro ponto interessante foi a questão de trabalhar com um cronograma de entregas e sprints. Seria interessante em uma das aulas falar sobre metodologias ágeis.<br><br>
+            Um ponto que talvez seja abordado em alguma futura aula de Mentoring seria a questão de como montar, preparar um currículo, seja no LinkedIn ou virtualmente para uma maior chance de contratação. Talvez seja algo abordado em Mentoring III.
+        </p>
+
+
+<?php include_once($pathFiles . "includes/footer.php") ?>
